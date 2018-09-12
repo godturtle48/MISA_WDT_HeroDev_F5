560b3e9e0309190f98e1207a6ebe8e6c5eff3380
= <33550236+trunghieu2609vn@users.noreply.github.com>
2018-09-12 15:23:35 +0700

    update Minh vs Hieu

--- NguyenThanhMinh/MISA.TeamUET/MISA.TeamUET/Scripts/home.js
+++ NguyenThanhMinh/MISA.TeamUET/MISA.TeamUET/Scripts/home.js
@@ -1,9 +1,27 @@
-﻿$('#icon-left-header').click(function () {
+﻿
+var check = 2;
+$('#icon-left-header').click(function () {
     $('.menu-right').toggle();
     $('.main').toggleClass('main-left-30px');
     $('.menu-li, .menu-li-ul-li').removeAttr('style');
     $('.menu-li-dropdown').filter('[status=show]').next().hide();
     $('.menu-li-dropdown').filter('[status=show]').removeAttr('status');
+
+    if (check%2 == 0) {
+        $('.search-box').css('min-width','390px');
+        $('#ip-search').css('width','80%');
+        $('.nav-panel-l-r').css('width','29.2%');
+        $('.setting-dialog').css('width','29.2%');
+        $('.nav-menu').css('box-shadow','7px -2px 36px -5px #003f7f');
+    }else{
+        $('.search-box').css('min-width','305px');
+        $('#ip-search').css('width','74%');
+        $('.nav-panel-l-r').css('width','24.2%');
+        $('.setting-dialog').css('width','26.6%');
+        $('.nav-menu').css('box-shadow','0 0 0 0');
+
+    }
+    check++;
 });
 
 // Thay đổi backgroup khi click vào 1 menu trong list menu bên trái
@@ -27,6 +45,7 @@ $('.menu-li-dropdown').click(function () {
         $(this).attr('status', 'show');
         $(this).find('span').removeClass("icon-menu-dropdown");
         $(this).find('span').addClass("icon-menu-dropup");
+
         debugger;
     } else {
         $(this).next().stop().slideUp(300);
