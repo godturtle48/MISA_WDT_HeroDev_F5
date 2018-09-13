7405ce2cf82c8e063695420662855a848637d989
Lê Lương Tuấn Anh <32769169+leluongtuananh@users.noreply.github.com>
2018-09-14 00:37:19 +0700

    phan thong tin nhan vien

---
+++ LeLuongTuanAnh/TuanAnh/Scripts/BTL.js
@@ -0,0 +1,26 @@
+﻿$('#show-info-toolbar-icon').click(function () {
+    if ($('#show-info-toolbar-detail').css('display') == 'none') {
+        $('#show-info-toolbar-detail').show(300,"swing");
+        
+    }
+    else {
+        $('#show-info-toolbar-detail').hide(300);
+        
+    }
+})
+$('#setting-toolbar-info-detail-icon').click(function () {
+    if ($('#setting-toolbar-info-detail-content').css('display') == 'none') {
+        $('#setting-toolbar-info-detail-content').show();
+
+    }
+    else {
+        $('#setting-toolbar-info-detail-content').hide();
+
+    }
+})
+$('#setting-toolbar-info-BT3').click(function () {    
+        $('#setting-toolbar-info-detail-content').hide();
+})
+$('#setting-toolbar-info-detail-closeBt').click(function () {
+    $('#setting-toolbar-info-detail-content').hide();
+})
