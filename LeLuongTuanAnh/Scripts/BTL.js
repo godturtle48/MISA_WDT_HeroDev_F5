2cc4cf37e61dea1686719275b6c39d07544707cb
leluongtuananh <leluongtuananh98@gmail.com>
2018-09-16 22:32:03 +0700

    sua border va hover

--- LeLuongTuanAnh/Scripts/BTL.js
+++ LeLuongTuanAnh/Scripts/BTL.js
@@ -11,18 +11,18 @@ $('#employee-info-table-all-button-control').click(function () {
         $('#table-info-all-employees').css('height', '266px');
         $('#employee-info-table-all-button-control').removeClass('employee-info-table-all-button2');
         $('#employee-info-table-all-button-control').addClass('employee-info-table-all-button');
-        $('#table-info-all-employees20').css('bottom', '239px');
-        $('#employee-info-table-footer').css('bottom', '238px');
-        $('#table-info-all-employees2').css('height', '168px');
+        $('#table-info-all-employees20').css('bottom', '241px');
+        $('#employee-info-table-footer').css('bottom', '241px');
+        $('#table-info-all-employees2').css('height', '173px');
     }
     else{
         $('#employee-general-info').hide();
         $('#table-info-all-employees').css('height', '516px');
         $('#employee-info-table-all-button-control').addClass('employee-info-table-all-button2');
         $('#employee-info-table-all-button-control').removeClass('employee-info-table-all-button');
-        $('#table-info-all-employees20').css('bottom', '489px');
-        $('#employee-info-table-footer').css('bottom', '490px');
-        $('#table-info-all-employees2').css('height', '419px');
+        $('#table-info-all-employees20').css('bottom', '491px');
+        $('#employee-info-table-footer').css('bottom', '491px');
+        $('#table-info-all-employees2').css('height', '424px');
     }
 });
 $('#show-info-toolbar-icon').click(function () {
@@ -56,11 +56,9 @@ $('#setting-toolbar-info-detail-closeBt').click(function () {
 $('.employee-info-toolbar-item').each(function (item, index) {
     $(this).click(function () {
         $('.employee-info-toolbar-item').each(function (item, index) {
-            $(this).css('border', '0px');
-            $(this).css('color', 'black');
+            $(this).removeClass('employee-info-toolbar-item-onselect');
         });
-        $(this).css('border-bottom', '3px solid #238DE6');
-        $(this).css('color', '#238DE6');
+        $(this).addClass('employee-info-toolbar-item-onselect');
         $('#employee-info-img').hide();
         $('#employee-info-maint-content').hide();
         if ($(this).attr('id') === 'employee-info-toolbar-item1') {
@@ -79,3 +77,163 @@ $('.employee-info-select-ion').each(function (item, index) {
         }
     });
 });
+$('#table-info-all-employees1-line1').hover(function () {
+    $('#table-info-all-employees2-line1').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line1').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line1').css('background','white');
+    $('#table-info-all-employees1-line1').css('background','white');
+}
+);
+$('#table-info-all-employees2-line1').hover(function () {
+    $('#table-info-all-employees2-line1').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line1').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line1').css('background','white');
+    $('#table-info-all-employees1-line1').css('background','white');
+}
+);
+$('#table-info-all-employees1-line2').hover(function () {
+    $('#table-info-all-employees2-line2').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line2').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line2').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line2').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees2-line2').hover(function () {
+    $('#table-info-all-employees2-line2').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line2').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line2').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line2').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees1-line3').hover(function () {
+    $('#table-info-all-employees2-line3').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line3').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line3').css('background','white');
+    $('#table-info-all-employees1-line3').css('background','white');
+}
+);
+$('#table-info-all-employees2-line3').hover(function () {
+    $('#table-info-all-employees2-line3').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line3').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line3').css('background','white');
+    $('#table-info-all-employees1-line3').css('background','white');
+}
+);
+$('#table-info-all-employees1-line4').hover(function () {
+    $('#table-info-all-employees2-line4').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line4').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line4').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line4').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees2-line4').hover(function () {
+    $('#table-info-all-employees2-line4').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line4').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line4').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line4').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees1-line5').hover(function () {
+    $('#table-info-all-employees2-line5').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line5').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line5').css('background','white');
+    $('#table-info-all-employees1-line5').css('background','white');
+}
+);
+$('#table-info-all-employees2-line5').hover(function () {
+    $('#table-info-all-employees2-line5').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line5').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line5').css('background','white');
+    $('#table-info-all-employees1-line5').css('background','white');
+}
+);
+$('#table-info-all-employees1-line6').hover(function () {
+    $('#table-info-all-employees2-line6').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line6').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line6').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line6').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees2-line6').hover(function () {
+    $('#table-info-all-employees2-line6').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line6').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line6').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line6').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees1-line7').hover(function () {
+    $('#table-info-all-employees2-line7').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line7').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line7').css('background','white');
+    $('#table-info-all-employees1-line7').css('background','white');
+}
+);
+$('#table-info-all-employees2-line7').hover(function () {
+    $('#table-info-all-employees2-line7').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line7').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line7').css('background','white');
+    $('#table-info-all-employees1-line7').css('background','white');
+}
+);
+$('#table-info-all-employees1-line8').hover(function () {
+    $('#table-info-all-employees2-line8').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line8').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line8').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line8').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees2-line8').hover(function () {
+    $('#table-info-all-employees2-line8').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line8').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line8').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line8').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees1-line9').hover(function () {
+    $('#table-info-all-employees2-line9').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line9').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line9').css('background','white');
+    $('#table-info-all-employees1-line9').css('background','white');
+}
+);
+$('#table-info-all-employees2-line9').hover(function () {
+    $('#table-info-all-employees2-line9').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line9').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line9').css('background','white');
+    $('#table-info-all-employees1-line9').css('background','white');
+}
+);
+$('#table-info-all-employees1-line10').hover(function () {
+    $('#table-info-all-employees2-line10').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line10').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line10').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line2').css('background','#F0F0F0');
+}
+);
+$('#table-info-all-employees2-line10').hover(function () {
+    $('#table-info-all-employees2-line10').css('background','#E7F4FF');
+    $('#table-info-all-employees1-line10').css('background','#E7F4FF');
+    },function () {
+    $('#table-info-all-employees2-line10').css('background','#F0F0F0');
+    $('#table-info-all-employees1-line10').css('background','#F0F0F0');
+}
+);
