d9f1e4b9e3a2eefeaa52d810bbcf3f6416a58b8c
leluongtuananh <leluongtuananh98@gmail.com>
2018-09-16 11:14:18 +0700

    sua

--- LeLuongTuanAnh/Scripts/BTL.js
+++ LeLuongTuanAnh/Scripts/BTL.js
@@ -1,17 +1,28 @@
-﻿$(document).ready(function () {
-    $('#table-info-all-employees2').scrollTop();
-});
+﻿
 $('#table-info-all-employees2').scroll(function () {
     $('#table-info-all-employees1').scrollTop($('#table-info-all-employees2').scrollTop());
 });
-$('#employee-info-table-all-button').click(function () {
-    if ($('#employee-general-info').css('display') == 'none') {
+$('#table-info-all-employees2').scroll(function () {
+    $('#table-info-all-employees21').scrollLeft($('#table-info-all-employees2').scrollLeft());
+});
+$('#employee-info-table-all-button-control').click(function () {
+    if ($('#employee-general-info').css('display') === 'none') {
         $('#employee-general-info').show();
-        $('#table-info-all-employees').css('height','319px');
+        $('#table-info-all-employees').css('height', '266px');
+        $('#employee-info-table-all-button-control').removeClass('employee-info-table-all-button2');
+        $('#employee-info-table-all-button-control').addClass('employee-info-table-all-button');
+        $('#table-info-all-employees20').css('bottom', '239px');
+        $('#employee-info-table-footer').css('bottom', '238px');
+        $('#table-info-all-employees2').css('height', '168px');
     }
     else{
         $('#employee-general-info').hide();
-        $('#table-info-all-employees').css('height','569px');
+        $('#table-info-all-employees').css('height', '516px');
+        $('#employee-info-table-all-button-control').addClass('employee-info-table-all-button2');
+        $('#employee-info-table-all-button-control').removeClass('employee-info-table-all-button');
+        $('#table-info-all-employees20').css('bottom', '489px');
+        $('#employee-info-table-footer').css('bottom', '490px');
+        $('#table-info-all-employees2').css('height', '419px');
     }
 });
 $('#show-info-toolbar-icon').click(function () {
@@ -52,7 +63,7 @@ $('.employee-info-toolbar-item').each(function (item, index) {
         $(this).css('color', '#238DE6');
         $('#employee-info-img').hide();
         $('#employee-info-maint-content').hide();
-        if ($(this).attr('id') == 'employee-info-toolbar-item1') {
+        if ($(this).attr('id') === 'employee-info-toolbar-item1') {
             $('#employee-info-img').show();
             $('#employee-info-maint-content').show();
         }
@@ -60,7 +71,7 @@ $('.employee-info-toolbar-item').each(function (item, index) {
 });
 $('.employee-info-select-ion').each(function (item, index) {
     $(this).click(function () {
-        if ($(this).next().css('display') == 'none') {
+        if ($(this).next().css('display') === 'none') {
             $(this).next().show();
         }
         else{
