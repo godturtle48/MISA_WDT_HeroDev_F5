593d4ba53418c2796fdab45608f3db004cfc176d
halfeti <halfeti10.1@gmail.com>
2018-09-21 13:42:37 +0700

    commit 21/09/2018

--- NguyenThiThuyNga/NguyenThiThuyNga_2/Scripts/index.js
+++ NguyenThiThuyNga/NguyenThiThuyNga_2/Scripts/index.js
@@ -26,11 +26,45 @@ let setupTooltip = function () {
 };
 setupTooltip();
 
-//$('.search-box').click(function () {
-//    $('.inactive-checkbox').toggle();
-//})
-
+//Kể từ đây đến hết là phần mới thêm
 $("#employee-file-add-button").click(function () {
     $('#employee-file').hide();
     $('#employee-file-add-button-pannel').show();
+})
+
+$('.arr-dropdown').click(function () {
+    $('.dropdown-content').toggle();
+})
+
+function filterFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('input-filter');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("contentfilter");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";         
+        }
+    }
+}
+$('.contentfilter li').click(function () {
+    var input = $(this).attr('value');
+    $('.dropdown-content').hide();
+    $('.input-filter').val(input);
+})
+$('.current-status').click(function () {
+    $('.right-right-status').toggle();
+})
+$('.right-right-status li').click(function () {
+    var input = $(this).attr('value');
+    $('.right-right-status').hide();
+    $('.current-status').val(input);
+
 })
